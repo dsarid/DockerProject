852348effee47a19790dab5b43e5efb11f072b62
Daniel S <daniel.sarid@tutanota.com>
2024-05-26 14:39:44 +0300

    mongodb turned-off

--- yolo5/app.py
+++ yolo5/app.py
@@ -130,14 +130,14 @@ def predict():
         logger.info(prediction_summary)
 
         # store the prediction_summary in MongoDB
-        try:
-            myclient = pymongo.MongoClient("mongodb://mongo1:27017/")
-            mydb = myclient["mydatabase"]
-            mycol = mydb["customers"]
-
-            x = mycol.insert_one(prediction_summary)
-        except Exception as e:
-            return str(e), 500
+        # try:
+        #     myclient = pymongo.MongoClient("mongodb://mongo1:27017/")
+        #     mydb = myclient["mydatabase"]
+        #     mycol = mydb["customers"]
+        #
+        #     x = mycol.insert_one(prediction_summary)
+        # except Exception as e:
+        #     return str(e), 500
 
         return convert_objectid(prediction_summary), 200
     else:
